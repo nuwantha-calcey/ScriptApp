af03a7add4b3f6979c441a2f6c2ce41affdacd9d
Nuwantha Sandakelum <nuwantha@calcey.com>
2023-08-14 16:12:15 +0530

    initial commit

---
+++ EmployeeManagement/Program.cs
@@ -0,0 +1,39 @@
+using EmployeeManagement;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+ 
+// add HttpClient support
+builder.Services.AddHttpClient("TopLevelManagementAPIClient");
+
+// add AutoMapper for mapping between entities and DTOs
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+// add other services
+builder.Services.RegisterBusinessServices();
+builder.Services.RegisterDataServices(builder.Configuration); 
+
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+app.MapHealthChecks("/health");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
